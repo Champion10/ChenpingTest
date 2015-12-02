f217502dee3260c5104eac26cc8f1459d1b04a21
张翼飞 <280772270@qq.com>
2015-12-02 21:42:10 +0800

    创建了初始文档

---
+++ Champion/Champion/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Champion
+//
+//  Created by 张翼飞 on 15/12/2.
+//  Copyright © 2015年 无限思维技术团队. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Champion/Champion/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Champion
+//
+//  Created by 张翼飞 on 15/12/2.
+//  Copyright © 2015年 无限思维技术团队. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
